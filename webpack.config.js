8385f8471d1361945985f5d56dc18f62f1d62641
Nathaniel Fredericks <40307305+nathfreder@users.noreply.github.com>
2018-09-27 20:44:53 -0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const WorkboxWebPackPlugin = require('workbox-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    open: true,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+    new WorkboxWebPackPlugin.GenerateSW({
+      swDest: 'serviceWorker.js',
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
+  ],
+};
